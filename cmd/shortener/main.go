34623f177fed7a1107e6a4ac038ba4d256b87873
PavelRyltsov <rltsv@yandex.ru>
2022-11-14 09:56:47 +0300

    add mod

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func MethodPost(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+
+	}
+}
+
+func main() {
+
+	h1 := http.HandlerFunc(MethodPost)
+
+	http.HandleFunc("/", h1)
+
+	http.ListenAndServe("localhost:8080", nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rltsv
+
+go 1.19
