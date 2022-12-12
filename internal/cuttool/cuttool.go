48452701e19c6a656c8718348466b6fa114b9529
PavelRyltsov <rltsv@yandex.ru>
2022-12-12 22:07:57 +0300

    changed status code in cuttool

--- internal/cuttool/cuttool.go
+++ internal/cuttool/cuttool.go
@@ -36,7 +36,7 @@ func MakeURLShorter(w http.ResponseWriter, r *http.Request) {
 
 	//Answer to client with shortened link
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(201)
 	_, err = w.Write([]byte(partOfURL))
 	if err != nil {
 		http.Error(w, err.Error(), 500)
