510cdbac5bb0489ca1aa68c23c7a48f26da27f01
PavelRyltsov <rltsv@yandex.ru>
2022-12-14 14:30:39 +0300

    put all code inside package main

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,16 +1,70 @@
 package main
 
 import (
-	"github.com/rltsv/internal/cuttool"
-	"github.com/rltsv/internal/urlgiver"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"sync"
 )
+
+var idCount = 0
+var URLStorage = make(map[int][]byte)
+var mux sync.Mutex
+
+func MakeShortLink(w http.ResponseWriter, r *http.Request) {
+	respBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Fatal("Ошибка: ", err)
+		return
+	}
+
+	//инкрементим счетчик айди на один и записываем полученную ссылку по данному айдишнику в хранилище
+	mux.Lock()
+	idCount++
+	URLStorage[idCount] = respBody
+	mux.Unlock()
+
+	//здесь сокращаем нашу ссылку, пока что будем делать просто порядковые номера: 1, 2...
+	var LinkAfterHashFunction string = strconv.Itoa(idCount)
+
+	//отвечаем клиенту в виде сокращенной ссылки и статус кодом
+	w.WriteHeader(201)
+	_, err = w.Write([]byte(LinkAfterHashFunction))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Fatal("Ошибка: ", err)
+		return
+	}
+}
+
+func GiveOriginalLinkToRequest(w http.ResponseWriter, r *http.Request) {
+	idValue := r.URL.Path
+	if idValue == "" {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+	_, idPart, _ := strings.Cut(idValue, "GET/")
+	intIDPart, err := strconv.Atoi(idPart)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	if _, ok := URLStorage[intIDPart]; !ok {
+		http.Error(w, "", http.StatusBadRequest)
+	}
+
+	w.Header().Set("Content-Location", string(URLStorage[intIDPart]))
+	w.WriteHeader(http.StatusTemporaryRedirect)
+
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/POST/", cuttool.MakeURLShorter)
-	mux.HandleFunc("/GET/", urlgiver.GetOrigURL)
+	mux.HandleFunc("/POST/", MakeShortLink)
+	mux.HandleFunc("/GET/", GiveOriginalLinkToRequest)
 
 	log.Fatal(http.ListenAndServe("localhost:8080", mux))
 }
